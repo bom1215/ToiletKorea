f1adb66e0e56a31329ae2774bf898cacd3ca5221
bom1215 <bom1215@naver.com>
2024-05-26 22:03:14 +0900

    chore: add hilt dependencies and kotlin classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -9,5 +10,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+
 }
 
