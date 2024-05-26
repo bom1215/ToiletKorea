0d2cdfe7903b0c97a2d028f267ec0a4131c4c851
bom1215 <bom1215@naver.com>
2024-05-26 22:20:48 +0900

    feat: Add AccountService interface

---
+++ app/src/main/java/com/example/toiletkorea/model/service/AccountService.kt
@@ -0,0 +1,18 @@
+package com.example.toiletkorea.model.service
+
+import kotlinx.coroutines.flow.Flow
+import com.example.toiletkorea.model.User
+
+interface AccountService {
+    val currentUserId: String
+    val hasUser: Boolean
+
+    val currentUser: Flow<User>
+
+    suspend fun authenticate(email: String, password: String)
+    suspend fun sendRecoveryEmail(email: String)
+    suspend fun createAnonymousAccount()
+    suspend fun linkAccount(email: String, password: String)
+    suspend fun deleteAccount()
+    suspend fun signOut()
+}
