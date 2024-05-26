713cfda3332fb42ca2cd3a84d6fbf0b9d5055e86
bom1215 <bom1215@naver.com>
2024-05-26 22:11:12 +0900

    feat: remove EmailAddress and current position value

--- app/src/main/java/com/example/toiletkorea/ui/login/LoginUiState.kt
+++ app/src/main/java/com/example/toiletkorea/ui/login/LoginUiState.kt
@@ -3,8 +3,6 @@ package com.example.toiletkorea.ui.login
 import com.google.android.gms.maps.model.LatLng
 
 data class LoginUiState(
-    val username : String = "",
-    val Password : String = "",
-    val EmailAddress : String = "",
-    val CurrentPosition : LatLng? = null,
+    val password : String = "",
+    val email : String = "",
 )
