13cbfd2c98fd41b2f22f13d9c566715892898344
bom1215 <bom1215@naver.com>
2024-06-02 23:08:03 +0900

    chore: change the Light colorScheme and discard the Dark colorScheme

--- app/src/main/java/com/example/toiletkorea/ui/theme/Color.kt
+++ app/src/main/java/com/example/toiletkorea/ui/theme/Color.kt
@@ -19,7 +19,13 @@ val md_theme_light_error = Color(0xFFBA1A1A)
 val md_theme_light_errorContainer = Color(0xFFFFDAD6)
 val md_theme_light_onError = Color(0xFFFFFFFF)
 val md_theme_light_onErrorContainer = Color(0xFF410002)
-val md_theme_light_background = Color(0xFFFBFDF9)
+
+
+val md_theme_light_background = Color(0xFFF5F5F5)
+
+
+
+
 val md_theme_light_onBackground = Color(0xFF191C1A)
 val md_theme_light_surface = Color(0xFFFBFDF9)
 val md_theme_light_onSurface = Color(0xFF191C1A)
--- app/src/main/java/com/example/toiletkorea/ui/theme/Theme.kt
+++ app/src/main/java/com/example/toiletkorea/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -48,37 +49,37 @@ private val LightColors = lightColorScheme(
 )
 
 
-private val DarkColors = darkColorScheme(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    primaryContainer = md_theme_dark_primaryContainer,
-    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    secondaryContainer = md_theme_dark_secondaryContainer,
-    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-    tertiary = md_theme_dark_tertiary,
-    onTertiary = md_theme_dark_onTertiary,
-    tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    error = md_theme_dark_error,
-    errorContainer = md_theme_dark_errorContainer,
-    onError = md_theme_dark_onError,
-    onErrorContainer = md_theme_dark_onErrorContainer,
-    background = md_theme_dark_background,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface,
-    surfaceVariant = md_theme_dark_surfaceVariant,
-    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-    outline = md_theme_dark_outline,
-    inverseOnSurface = md_theme_dark_inverseOnSurface,
-    inverseSurface = md_theme_dark_inverseSurface,
-    inversePrimary = md_theme_dark_inversePrimary,
-    surfaceTint = md_theme_dark_surfaceTint,
-    outlineVariant = md_theme_dark_outlineVariant,
-    scrim = md_theme_dark_scrim,
-)
+//private val DarkColors = darkColorScheme(
+//    primary = md_theme_dark_primary,
+//    onPrimary = md_theme_dark_onPrimary,
+//    primaryContainer = md_theme_dark_primaryContainer,
+//    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+//    secondary = md_theme_dark_secondary,
+//    onSecondary = md_theme_dark_onSecondary,
+//    secondaryContainer = md_theme_dark_secondaryContainer,
+//    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+//    tertiary = md_theme_dark_tertiary,
+//    onTertiary = md_theme_dark_onTertiary,
+//    tertiaryContainer = md_theme_dark_tertiaryContainer,
+//    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+//    error = md_theme_dark_error,
+//    errorContainer = md_theme_dark_errorContainer,
+//    onError = md_theme_dark_onError,
+//    onErrorContainer = md_theme_dark_onErrorContainer,
+//    background = md_theme_dark_background,
+//    onBackground = md_theme_dark_onBackground,
+//    surface = md_theme_dark_surface,
+//    onSurface = md_theme_dark_onSurface,
+//    surfaceVariant = md_theme_dark_surfaceVariant,
+//    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+//    outline = md_theme_dark_outline,
+//    inverseOnSurface = md_theme_dark_inverseOnSurface,
+//    inverseSurface = md_theme_dark_inverseSurface,
+//    inversePrimary = md_theme_dark_inversePrimary,
+//    surfaceTint = md_theme_dark_surfaceTint,
+//    outlineVariant = md_theme_dark_outlineVariant,
+//    scrim = md_theme_dark_scrim,
+//)
 
 @Composable
 fun ToiletKoreaTheme(
@@ -87,15 +88,16 @@ fun ToiletKoreaTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//            val context = LocalContext.current
-//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//        }
-
-        darkTheme -> DarkColors
-        else -> LightColors
-    }
+//    val colorScheme = when {
+////        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+////            val context = LocalContext.current
+////            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+////        }
+//
+//        darkTheme -> DarkColors
+//        else -> LightColors
+//    }
+    val colorScheme = LightColors
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
