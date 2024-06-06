7701319f4e5f038323f2690a0c0c69866a81ea27
bom1215 <bom1215@naver.com>
2024-06-07 01:10:46 +0900

    chore: change RoundedCornerShape

--- app/src/main/java/com/example/toiletkorea/ui/theme/Shape.kt
+++ app/src/main/java/com/example/toiletkorea/ui/theme/Shape.kt
@@ -5,6 +5,6 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(50.dp),
+    small = RoundedCornerShape(27),
     medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
 )
