5c4b45a5ed8d97fedbb9b134be34af734eabbdf8
bom1215 <bom1215@naver.com>
2024-06-07 01:11:22 +0900

    Feat: Add new LoginTheme

--- app/src/main/java/com/example/toiletkorea/ui/theme/Theme.kt
+++ app/src/main/java/com/example/toiletkorea/ui/theme/Theme.kt
@@ -2,19 +2,36 @@ package com.example.toiletkorea.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
+import com.example.toiletkorea.R
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -112,4 +129,44 @@ fun ToiletKoreaTheme(
         content = content,
         shapes = Shapes,
     )
+}
+
+
+@Composable
+fun LoginTheme(@StringRes title: Int, @StringRes subtitle: Int, ) {
+    Box(
+        modifier = Modifier
+            .size(width = 292.dp, height = 186.dp)
+    )
+    {
+        Image(
+            painterResource(id = R.drawable.main_logo),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(80.dp)
+                .align(Alignment.TopCenter)
+        )
+        Column(modifier = Modifier.align(Alignment.BottomStart)) {
+            Text(
+                text = stringResource(id = title),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.displayLarge
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = stringResource(id = subtitle),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                style = MaterialTheme.typography.displaySmall
+            )
+        }
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun LoginThemePreview(){
+    LoginTheme(title  = R.string.welcome_back, subtitle = R.string.login_to_continue)
 }
