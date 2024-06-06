626489f97927060a6ccce3b64f9bcde33d05b239
bom1215 <bom1215@naver.com>
2024-06-07 01:16:25 +0900

    Feat: Change components on EmailField and PasswordField

--- app/src/main/java/com/example/toiletkorea/ui/composable/TextField.kt
+++ app/src/main/java/com/example/toiletkorea/ui/composable/TextField.kt
@@ -1,36 +1,79 @@
 package com.example.toiletkorea.ui.composable
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import com.example.toiletkorea.R
 
+
+
+@Composable
+fun GeneralTextField(@StringRes title: Int, value: String, onNewValue: (String) -> Unit, modifier: Modifier = Modifier) {
+    Column(){
+        Text(text = stringResource(title))
+        Spacer(modifier = Modifier.size(7.dp))
+        TextField(
+            singleLine = true,
+            modifier = modifier.size(width = 292.dp, height = 47.dp),
+            shape = MaterialTheme.shapes.small,
+            value = value,
+            onValueChange = { onNewValue(it) },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+        )
+    }
+
+}
 @Composable
 fun EmailField(value: String, onNewValue: (String) -> Unit, modifier: Modifier = Modifier) {
-    OutlinedTextField(
-        singleLine = true,
-        modifier = modifier,
-        value = value,
-        onValueChange = { onNewValue(it) },
-        placeholder = { Text(stringResource(R.string.email)) },
-//        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email") }
-    )
+    Column(){
+        Text("Email address")
+        Spacer(modifier = Modifier.size(7.dp))
+        TextField(
+            singleLine = true,
+            modifier = modifier.size(width = 292.dp, height = 47.dp),
+            shape = MaterialTheme.shapes.small,
+            value = value,
+            onValueChange = { onNewValue(it) },
+//            placeholder = { Text(stringResource(R.string.email)) },
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email") },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+        )
+    }
+
 }
 
 @Composable
@@ -56,25 +99,36 @@ private fun PasswordField(
 ) {
     var isVisible by remember { mutableStateOf(false) }
 
-//    val icon =
-//        if (isVisible) painterResource(AppIcon.ic_visibility_on)
-//        else painterResource(AppIcon.ic_visibility_off)
+    val icon =
+        if (isVisible) painterResource(R.drawable.visibility_on)
+        else painterResource(R.drawable.visibility_off)
 
     val visualTransformation =
         if (isVisible) VisualTransformation.None else PasswordVisualTransformation()
 
-    OutlinedTextField(
-        modifier = modifier,
-        value = value,
-        onValueChange = { onNewValue(it) },
-        placeholder = { Text(text = stringResource(placeholder)) },
-//        leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock") },
-//        trailingIcon = {
-//            IconButton(onClick = { isVisible = !isVisible }) {
-//                Icon(painter = icon, contentDescription = "Visibility")
-//            }
-//        },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        visualTransformation = visualTransformation
-    )
+    Column() {
+        Text("Password")
+        Spacer(modifier = Modifier.size(7.dp))
+        TextField(
+            modifier = modifier.size(width = 292.dp, height = 47.dp),
+            shape = MaterialTheme.shapes.small,
+            value = value,
+            onValueChange = { onNewValue(it) },
+//            placeholder = { Text(text = stringResource(placeholder)) },
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock") },
+            trailingIcon = {
+                IconButton(onClick = { isVisible = !isVisible }) {
+                    Icon(painter = icon, contentDescription = "Visibility")
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = visualTransformation,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+    }
+
 }
