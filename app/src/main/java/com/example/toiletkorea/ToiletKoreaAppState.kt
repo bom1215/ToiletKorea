be5c5e4e3fcb508faa9aac7b44bfd0914c4f34d5
bom1215 <bom1215@naver.com>
2024-06-09 17:56:05 +0900

    Feat: add toiletkorea appstate

---
+++ app/src/main/java/com/example/toiletkorea/ToiletKoreaAppState.kt
@@ -0,0 +1,52 @@
+package com.example.toiletkorea
+
+import android.content.res.Resources
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Stable
+import androidx.navigation.NavHostController
+import com.example.toiletkorea.model.snackbar.SnackbarManager
+import com.example.toiletkorea.model.snackbar.SnackbarMessage.Companion.toMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
+
+@Stable
+class ToiletKoreaAppState(
+    val snackbarHostState: SnackbarHostState,
+    val navController: NavHostController,
+    private val snackbarManager: SnackbarManager,
+    private val resources: Resources,
+    coroutineScope: CoroutineScope
+) {
+    init {
+        coroutineScope.launch {
+            snackbarManager.snackbarMessages.filterNotNull().collect { snackbarMessage ->
+                val text = snackbarMessage.toMessage(resources)
+                snackbarHostState.showSnackbar(text)
+                snackbarManager.clearSnackbarState()
+            }
+        }
+    }
+
+    fun popUp() {
+        navController.popBackStack()
+    }
+
+    fun navigate(route: String) {
+        navController.navigate(route) { launchSingleTop = true }
+    }
+
+    fun navigateAndPopUp(route: String, popUp: String) {
+        navController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(popUp) { inclusive = true }
+        }
+    }
+
+    fun clearAndNavigate(route: String) {
+        navController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(0) { inclusive = true }
+        }
+    }
+}
