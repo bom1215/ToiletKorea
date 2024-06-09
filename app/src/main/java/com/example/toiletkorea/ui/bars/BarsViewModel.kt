ce56baff516b10b125caf2a2d4c5e20de587c057
bom1215 <bom1215@naver.com>
2024-06-09 17:41:14 +0900

    Feat: Add new features on sidebar

--- app/src/main/java/com/example/toiletkorea/ui/bars/BarsViewModel.kt
+++ app/src/main/java/com/example/toiletkorea/ui/bars/BarsViewModel.kt
@@ -1,26 +1,21 @@
 package com.example.toiletkorea.ui.bars
 
-import android.util.Log
-import com.example.toiletkorea.TAG
+import SettingsUiState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import com.example.makeitso.screens.sign_up.SignUpUiState
 import com.example.toiletkorea.model.service.AccountService
 import com.example.toiletkorea.ui.ToiletKoreaViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class BarsViewModel @Inject constructor(
     private val accountService: AccountService
 ) : ToiletKoreaViewModel() {
+    val uiState = accountService.currentUser.map {
+        BarsUiState(isAnonymousAccount = it.isAnonymous, username = it.username) }
 
-    fun onSignOutClick(){
-        launchCatching {
-            try {
-                accountService.signOut()
-                Log.d(TAG, "sign Out Complete")
-
-            }catch (e : Exception){
-                Log.d(TAG, "signOut Error", e)
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/toiletkorea/ui/bars/bars.kt
+++ app/src/main/java/com/example/toiletkorea/ui/bars/bars.kt
@@ -1,6 +1,15 @@
 package com.example.toiletkorea.ui.bars
 
+import SettingsUiState
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -16,15 +25,28 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.toiletkorea.R
+import com.example.toiletkorea.ToiletScreen
+import com.example.toiletkorea.ui.composable.ShareButton
+import com.example.toiletkorea.ui.composable.SidebarButton
+import com.example.toiletkorea.ui.settings.SettingsViewModel
 import com.example.toiletkorea.ui.theme.ToiletKoreaTheme
 import kotlinx.coroutines.launch
 
@@ -61,61 +83,109 @@ fun ToiletTopBar(
 }
 
 @Composable
-fun SidebarInfo(
+fun Sidebar(
     modifier: Modifier = Modifier,
     navController: NavController,
-    viewModel: BarsViewModel = hiltViewModel()
-
 ){
-    ModalDrawerSheet {
-
-        TextButton(onClick = { /*TODO*/ }) {
-            Icon(
-                painter = painterResource(R.drawable.profile),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.size(30.dp))
-            Text(text = "name",
-                style = MaterialTheme.typography.displayMedium)
-        }
-        Spacer(modifier = Modifier.size(30.dp))
-        TextButton(onClick = { /*TODO*/ }) {
-            Icon(
-                painter = painterResource(R.drawable.main_page),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.size(60.dp))
-            Text(text = "Main Page",
-                style = MaterialTheme.typography.labelSmall)
-        }
-        TextButton(onClick = { /*TODO*/ }) {
-            Icon(
-                painter = painterResource(R.drawable.notice),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.size(60.dp))
-            Text(text = "Notices",
-                style = MaterialTheme.typography.labelSmall)
-        }
-        TextButton(onClick = {
-            viewModel.onSignOutClick()
-        }) {
-            Icon(
-                painter = painterResource(R.drawable.logout),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.size(60.dp))
-            Text(text = "Log Out",
-                style = MaterialTheme.typography.labelSmall)
-        }
+    ModalDrawerSheet(
+        modifier = modifier,
+        drawerContainerColor = MaterialTheme.colorScheme.primaryContainer,
+        drawerShape = RectangleShape
+    ) {
+        Spacer(modifier = Modifier.size(26.dp))
+        SidebarProfile()
+        Spacer(modifier = Modifier.size(17.dp))
+        SidebarOption(
+            onSettingsClick = {navController.navigate(ToiletScreen.SettingMain.name)},
+            onMainPageClick = {navController.navigate(ToiletScreen.Map.name)})
     }
 }
 
-
-@Preview
 @Composable
-fun SidebarInfo(){
-    ToiletKoreaTheme {
-        SidebarInfo(navController  = rememberNavController())
+fun SidebarProfile(){
+    val viewModel: BarsViewModel = hiltViewModel()
+
+    val uiState by viewModel.uiState.collectAsState(initial = BarsUiState(false))
+
+    val displayName = if (uiState.isAnonymousAccount || uiState.username.isNullOrEmpty()) {
+        "Guest"
+    } else {
+        uiState.username
+    }
+    Row(
+        modifier = Modifier
+            .size(width = 164.dp, height = 43.dp)
+            .padding(start = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Box(
+            modifier = Modifier
+                .size(43.dp)
+                .clip(shape = MaterialTheme.shapes.small)
+                .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                )
+        {Icon(
+            painter = painterResource(id = R.drawable.profile ),
+            contentDescription = null,
+            tint = Color.Unspecified,
+            modifier = Modifier.align(Alignment.Center)
+            )
+        }
+        Spacer(modifier = Modifier.size(9.dp))
+        Text(
+            text = displayName!!,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.displayMedium,
+            color = Color.Black
+        )
+
+
     }
 }
+
+
+@Composable
+fun SidebarOption(
+    modifier: Modifier = Modifier,
+    onMainPageClick: () -> Unit,
+    onSettingsClick: () -> Unit,
+    ){
+    Column(
+        modifier = modifier.size(width = 233.dp, height = 170.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        SidebarButton(text = R.string.main_page, icon = R.drawable.main_page, action = {onMainPageClick()})
+        SidebarButton(text = R.string.settings, icon = R.drawable.settings, action = {onSettingsClick()})
+        ShareButton(text = R.string.share, icon = R.drawable.share, url = R.string.share_url, context = LocalContext.current)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SidebarPreview(){
+    ToiletKoreaTheme {
+        Sidebar(navController  = rememberNavController())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SidebarProfilePreview(){
+    ToiletKoreaTheme {
+        SidebarProfile()
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun SidebarOptionPreview(){
+    ToiletKoreaTheme {
+        SidebarOption(
+            onMainPageClick = {},
+            onSettingsClick = {}
+        )
+    }
+}
+
+
