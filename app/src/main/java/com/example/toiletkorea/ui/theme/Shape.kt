dc63c1a1c30ab63f309cb33d4db5532a452fe727
bom1215 <bom1215@naver.com>
2024-06-09 17:54:46 +0900

    chore : add new shape and elements of data class

--- app/src/main/java/com/example/toiletkorea/ui/theme/Shape.kt
+++ app/src/main/java/com/example/toiletkorea/ui/theme/Shape.kt
@@ -5,6 +5,8 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(10),
     small = RoundedCornerShape(27),
-    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
+//    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp),
+    large = RoundedCornerShape(47)
 )
